babe98ee14187d13d818d75f935553bd71329672
nirmal3002 <nirmal.m3002@gmail.com>
2024-09-30 05:40:21 +0000

    the 1st update in backend prog

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const bookRoutes = require('./routes/clinicroutes');
+const clinicRoutes = require('./routes/clinicroutes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 connectDB();
@@ -8,11 +8,28 @@ connectDB();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("HomePage Of The App");
+    res.send("HomePage");
+});
+app.post('/', (req, res) => {
+     res.send('Post received!');
 });
 
-app.use('/api', clinics); // Use book routes with prefix '/api'
+app.use('/api', clinicRoutes); // Use book routes with prefix '/api'
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
+// const express = require('express');
+// const app = express();
+
+// // Middleware to parse JSON bodies
+// app.use(express.json());
+
+// app.post('/', (req, res) => {
+//     // Handle the POST request here
+//     res.send('Post received!');
+// });
+
+// app.listen(3000, () => {
+//     console.log('Server is running on port 3000');
+// });
