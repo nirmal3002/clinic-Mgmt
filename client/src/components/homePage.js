5495a4d4952edd7bd6ddef0dfab686fc24eecbe0
nirmal3002 <nirmal.m3002@gmail.com>
2024-12-25 04:54:23 +0000

    prod deploy:updated home page design

--- client/src/components/homePage.js
+++ client/src/components/homePage.js
@@ -1,55 +1,3 @@
-
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import { Container, Typography, Button, Box } from '@mui/material';
-// import GitHubIcon from '@mui/icons-material/GitHub';
-
-// const HomePage = () => {
-//     return (
-//      <Container maxWidth="lg" sx={{ textAlign: 'center', py: 5 }}>
-//         <Typography variant="h2" component="h1" color="success">
-//           Welcome to the Clinic Management System
-//         </Typography>
-//         <Typography variant="h5" gutterBottom>
-//           Manage Patients Efficiently
-//         </Typography>
-//         <Box mt={4}>
-//         <Button component={Link} to="/list" color="primary" 
-//           variant="contained" >
-//           View Patients
-//         </Button>
-//         <Button component={Link} to="/export" color="primary" 
-//           variant="contained" >
-//           Download Patients-list
-//         </Button>
-//         <Button
-//               color="primary"
-//               component="a"
-//               href="https://github.com/nirmal3002/clinic-Mgmt"
-//               target="_blank"
-//                variant="contained"
-//               rel="noopener noreferrer"
-//              >
-//               <GitHubIcon />
-//               GitHub
-//               </Button>
-//               <Button
-//               color="primary"
-//               component="a"
-//               href="https://docs.google.com/document/d/1951CLEB80bJ5kHb3fJa355BsURLlvO9wbkWmdBRDtbk/edit?tab=t.0"
-//               target="_blank"
-//                variant="contained"
-//               rel="noopener noreferrer"
-//              >
-//               My Resume
-//              </Button>
-
-//       </Box>
-//        </Container>
-//   );
-// };
-
-// export default HomePage;
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Typography, Button, Box, Grid } from '@mui/material';
@@ -59,7 +7,20 @@ import SearchIcon from '@mui/icons-material/Search';
 const HomePage = () => {
   return (
     
-    <Container maxWidth="lg" sx={{ textAlign: 'center', py: 5 }}>
+    <Container maxWidth="lg" sx={{
+       textAlign: 'center',
+       py: 5 ,
+      position: 'relative',
+        height: '100%',
+        width: '100%',
+        overflow: 'hidden',
+        backgroundImage: 'url(https://media.gettyimages.com/id/1312706413/photo/modern-hospital-building.jpg?s=1024x1024&w=gi&k=20&c=2nU8Ac2_g9NiiRTgZXfBqSRx50tR4x8R7io7X1OCUFg=)',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundAttachment: 'fixed',
+        filter: 'brightness(65%)'
+    }}>
       <Typography variant="h2" component="h1" color="success" gutterBottom>
         Welcome to the Clinic Management System
       </Typography>
